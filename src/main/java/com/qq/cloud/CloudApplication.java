16a2e8c279582e82ef7b28c61e904c49a80c6019
AndrewZ <8wspace@gmail.com>
2020-06-12 15:58:59 +0300

    Init

---
+++ src/main/java/com/qq/cloud/CloudApplication.java
@@ -0,0 +1,21 @@
+package com.qq.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController		
+public class CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!4", name);
+	}
+}
